84e92047f84347c495892c8610848691cbdfbbf7
KornelKopanski <kkornel1771@gmail.com>
2019-08-14 12:41:32 +0200

    Usunięto zbędne pliki

--- git_account_one_Tkinter/new_window.py
+++ git_account_one_Tkinter/new_window.py
@@ -1,7 +1,6 @@
 
-from git_account_one_Tkinter import button_account_one,elelments,category_slider,add_product
+from git_account_one_Tkinter import button_account_one,elelments,category_slider
 
-shopping = {}
 
 class Windows:
 
@@ -27,8 +26,6 @@ class Windows:
 
         product_button = button_account_one.CreateButtonsTwo(self.tk,window_two)
         product_button.add_product_button()
-
-        add = add_product.AddProduct(product_button.)
 
         # Etykieta na produkkty
         elements_etiquette = elelments.Elements(self.tk,self.root,window_two)
